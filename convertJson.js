f63b8499f180cc85163970efc067206d348965d1
Max Tham <70890998+MaxTham@users.noreply.github.com>
2025-02-28 18:59:29 +0800

    Update convertJson.js

--- convertJson.js
+++ convertJson.js
@@ -2,7 +2,7 @@ const xlsx = require("xlsx");
 const fs = require("fs");
 
 // Load the Excel file
-const workbook = xlsx.readFile("./coin_list.xlsx");
+const workbook = xlsx.readFile("./The Path to Your Excel);
 
 // Access the first sheet (or specify the sheet name if needed)
 const sheet = workbook.Sheets[workbook.SheetNames[0]];
@@ -11,7 +11,7 @@ const sheet = workbook.Sheets[workbook.SheetNames[0]];
 const data = xlsx.utils.sheet_to_json(sheet);
 
 // Extract the symbols column (replace 'symbol' with your column header name)
-const symbolsArray = data.map(row => row["Symbol"]); // Make sure "symbol" matches the column header in Excel
+const symbolsArray = data.map(row => row["The header"]); // Make sure it matches the column header in Excel
 
 // Structure it into the desired JSON format
 const result = {
